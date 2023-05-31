8c3f31d6aef534284792935735ae027f78e771f5
Jordan Clayton <jordan.clayton@torontomu.ca>
2023-05-31 19:27:57 -0400

    task model rdy for testing; next - write unit tests; reminder need flesh out

---
+++ lib/test/task_tester.dart
@@ -0,0 +1,10 @@
+class TaskTester
+{
+  void main()
+  {
+    /// TODO: Implement a suite of tests:
+    /// -Task object creation
+    /// -Collections behaviour: sorting, reordering, add/remove
+    /// -Completion and event handling.
+  }
+}
